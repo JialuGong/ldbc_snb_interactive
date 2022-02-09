660470bff355f560f9190fa685b46a2d73827f99
huaiyu <huaiyugong123@gmail.com>
2022-02-09 20:52:25 +0800

    fix: no  $NEO4J_CONTAINER_NAME var in cypher creat-indices.sh

--- cypher/scripts/create-indices.sh
+++ cypher/scripts/create-indices.sh
@@ -4,5 +4,8 @@ set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/vars.sh
 
 docker exec --interactive ${NEO4J_CONTAINER_NAME} cypher-shell < indices.cypher
